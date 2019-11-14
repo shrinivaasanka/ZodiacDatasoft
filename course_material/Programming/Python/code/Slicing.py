b42b4faa5cd5849430937437c28188656b4931b4
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2019-11-14 16:54:33 +0530

    Multidimensional Array Slicing - slice() function and NumPy - and StringIO

---
+++ course_material/Programming/Python/code/Slicing.py
@@ -0,0 +1,30 @@
+##############################################################################################################################################
+#<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+###############################################################################################################################################
+#Course Authored By:
+#-----------------------------------------------------------------------------------------------------------
+#K.Srinivasan
+#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+#Personal website(research): https://sites.google.com/site/kuja27/
+#-----------------------------------------------------------------------------------------------------------
+##############################################################################################################################################
+
+from io import StringIO
+import numpy as np
+
+if __name__=="__main__":
+	text=u"1,2,3,4,5,6,7,8\n9,10,11,12,13,14,15,16\n"
+	parsedarray=np.genfromtxt(StringIO(text),delimiter=",")
+	print(parsedarray)
+	slicedarray1=parsedarray[slice(0,3),slice(0,3)]
+	print("slicedarray1 - slice() of equal slices:")
+	print(slicedarray1)
+	slicedarray2=parsedarray[slice(0,2),slice(0,3)]
+	print("slicedarray2 - slice() of unequal slices:")
+	print(slicedarray2)
+	slicedarray3=parsedarray[slice(0,3),(0,3)]
+	print("slicedarray3 - slice() and tuple:")
+	print(slicedarray3)
+	slicedarray4=parsedarray[slice(0,7,2),slice(0,7,2)]
+	print("slicedarray4 - step slice() :")
+	print(slicedarray4)
