9bb33e9de0cef3bf83822bdce55a9c00e113d7a8
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2019-12-14 15:40:53 +0530

    14 December 2019 - Rounding off, Floating point division, Python 2.7 and Python 3.7.5, PDB

---
+++ course_material/Programming/Python/code/RoundOff.py
@@ -0,0 +1,21 @@
+##############################################################################################################################################
+# <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+###############################################################################################################################################
+# Course Authored By:
+# -----------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://sites.google.com/site/kuja27/
+# -----------------------------------------------------------------------------------------------------------
+##############################################################################################################################################
+
+
+class RoundOff:
+    def __init__(self):
+        x1 = (1-2)/2
+        x2 = (2-1)/2
+        print("x1:", x1)
+        print("x2:", x2)
+
+if __name__=="__main__":
+    ro=RoundOff()
