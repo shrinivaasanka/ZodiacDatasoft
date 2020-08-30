de2ac669b54e16519c0918dee1738e593d5740a7
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2020-08-30 18:49:23 +0530

    863. (THEORY and FEATURE) MAVSDK Python jMAVSIM Drone flight simulator - Proof-of-Concept implementation related to 862 and all sections on Drone Autonomous Delivery, Drone EVMs, Large Scale Visuals/Urban Sprawl/GIS Analytics, VIRGO PXRC Flight controller kernel driver - 26 August 2020

--- course_material/Programming/Python/code/Drone_MAVSDK_Client.py
+++ course_material/Programming/Python/code/Drone_MAVSDK_Client.py
@@ -13,6 +13,7 @@ import asyncio
 import sys
 from mavsdk import System
 
+
 class DroneMAVSDKClient(asyncio.Protocol):
     def __init__(self, message, on_con_lost, clientloop):
         self.message = message
@@ -32,36 +33,54 @@ class DroneMAVSDKClient(asyncio.Protocol):
     def connection_lost(self, cl):
         print("Connection lost")
 
-    async def drone_async_io(self,drone,mavsdkserverport,command):
+    async def drone_async_io(self, drone, mavsdkserverport, command):
         await drone.connect(system_address="udp://:"+str(mavsdkserverport))
+        print("drone_async_io(): Connected to MAVSDK server")
+        async for state in drone.core.connection_state():
+            print("state:",state)
+            if state.is_connected:
+                print(f"Drone discovered with UUID: {state.uuid}",state.uuid)
+                break
+        async for health in drone.telemetry.health():
+            if health.is_global_position_ok:
+                print("Global position estimate ok")
+                break
+        if command == "all":
+            await drone.info.get_flight_information()
+            await drone.action.arm()
+            await drone.action.takeoff()
+            await drone.camera.start_video_streaming()
+            await drone.action.goto_location(78.0, 7.0, 100.0, 0.0)
+            await drone.action.land()
         if command == "info":
             try:
-                print("flight information:",await drone.info.get_flight_information())
-                print("flight id:",await drone.info.get_identification())
-                print("speed:",await drone.info.get_speed_factor())
-                print("version:",await drone.info.get_version())
+                print("flight information:", await drone.info.get_flight_information())
+                print("flight id:", await drone.info.get_identification())
+                print("speed:", await drone.info.get_speed_factor())
+                print("version:", await drone.info.get_version())
             except Exception as ex:
-                print("Exception:",ex)
-        if command == "arm": 
+                print("Exception:", ex)
+        if command == "arm":
             try:
                 await drone.action.arm()
             except Exception as ex:
-                print("Exception:",ex)
-        if command == "takeoff": 
+                print("Exception:", ex)
+        if command == "takeoff":
             try:
                 await drone.action.takeoff()
             except Exception as ex:
-                print("Exception:",ex)
+                print("Exception:", ex)
         if command == "goto_location":
             try:
-                await drone.action.goto_location(78.0,7.0,100.0,0.0)
+                await drone.action.goto_location(78.0, 7.0, 100.0, 0.0)
             except Exception as ex:
-                print("Exception:",ex)
+                print("Exception:", ex)
         if command == "camera":
             try:
                 await drone.camera.start_video_streaming()
             except Exception as ex:
-                print("Exception:",ex)
+                print("Exception:", ex)
+
 
 async def loop_print():
     cnt = 0
@@ -70,18 +89,20 @@ async def loop_print():
     cnt += 1
     return
 
+
 async def main():
     clientloop = asyncio.get_running_loop()
     on_con_lost = clientloop.create_future()
-    dronemavsdkclient = DroneMAVSDKClient(input(), on_con_lost, clientloop)
+    dronemavsdkclient = DroneMAVSDKClient("DroneMAVSDKClient", on_con_lost, clientloop)
     transport, protocol = await clientloop.create_connection(lambda: dronemavsdkclient, 'localhost', 20000)
     drone = System()
-    await dronemavsdkclient.drone_async_io(drone,14540,command="info")
-    await dronemavsdkclient.drone_async_io(drone,14540,command="takeoff")
-    await dronemavsdkclient.drone_async_io(drone,14540,command="arm")
-    await dronemavsdkclient.drone_async_io(drone,14540,command="camera")
-    await dronemavsdkclient.drone_async_io(drone,14540,command="goto_location")
-    #await on_con_lost
+    await dronemavsdkclient.drone_async_io(drone, 14540, command="all")
+    #await dronemavsdkclient.drone_async_io(drone, 14540, command="info")
+    #await dronemavsdkclient.drone_async_io(drone, 14540, command="arm")
+    #await dronemavsdkclient.drone_async_io(drone, 14540, command="takeoff")
+    #await dronemavsdkclient.drone_async_io(drone, 14540, command="camera")
+    #await dronemavsdkclient.drone_async_io(drone, 14540, command="goto_location")
+    # await on_con_lost
 
 if __name__ == "__main__":
     asyncio.run(main())
