fb132669980808e83f97ec63314401e7eccb7073
Krishna iResearch - NeuronRain - K.Srinivasan <ka.shrinivaasan@gmail.com>
2021-07-26 22:01:26 +0530

    (*) 1153. (THEORY and FEATURE) ThoughtNet Evocation of Thought Hyperedges by Set Intersection of Hypervertices - related to 620,1147,1149 and all sections of ThoughtNet Hypergraph Evocation, Contextual Multiarmed Bandits, Reinforcement Learning, Word Sense Disambiguation, Balls-Bins Problem - NeuronRain implementation of set intersection by hashing - 26 July 2021 (*) 828 updated - Scaling Large Numbers and Social Networks

--- course_material/Programming/Python/code/HypergraphIntersections.py
+++ course_material/Programming/Python/code/HypergraphIntersections.py
@@ -2,17 +2,32 @@ import sys
 import numpy as np
 from collections import defaultdict
 
-def hypervertices_intersections(hypergraph,intersectinghypervertices):
-    hyperedges=hypergraph.values()
-    intersections=set(hypergraph[intersectinghypervertices[0]])
-    for ishv in intersectinghypervertices:
-            v1 = hypergraph[ishv]
-            setv1=set(v1)
-            intersections=intersections.intersection(setv1)
-    print('hypervertices intersection of ',intersectinghypervertices,':',intersections)
+def hypervertices_intersections(hypergraph,intersectinghypervertices, neuronrain_intersection=True):
+	hyperedges=hypergraph.values()
+	intersections=set(hypergraph[intersectinghypervertices[0]])
+	for ishv in intersectinghypervertices:
+		v1 = hypergraph[ishv]
+		setv1=set(v1)
+		if neuronrain_intersection == True:
+			intersections=neuronrain_setintersection(intersections,setv1)
+		else:
+			intersections=intersections.intersection(setv1)
+	print('hypervertices intersection of ',intersectinghypervertices,':',intersections)
+
+def neuronrain_setintersection(set1,set2):
+	set2dict=defaultdict()
+	cnt=0
+	for element in set2:
+		set2dict[cnt]=element
+		cnt+=1
+	intersection=[]
+	for element in set1:
+		if element in set2dict.values():
+			intersection.append(element)
+	return set(intersection)
 
 if __name__=="__main__":
-    thoughtnet_hypergraph={'water':['flood caused by storms wrought havoc','water scarcity hits crops','cost effective sea water desalination would benefit agriculture'],'air':['flood caused by storms wrought havoc','air traffic flourished in last financial year','air pollution hits new high'],'agriculture':['water scarcity hits crops','cost effective sea water desalination would benefit agriculture']}
-    thoughtnetevocations=hypervertices_intersections(thoughtnet_hypergraph,['water','air'])
-    thoughtnetevocations=hypervertices_intersections(thoughtnet_hypergraph,['water','agriculture'])
-    thoughtnetevocations=hypervertices_intersections(thoughtnet_hypergraph,['water','air','agriculture'])
+	thoughtnet_hypergraph={'water':['flood caused by storms wrought havoc','water scarcity hits crops','cost effective sea water desalination would benefit agriculture'],'air':['flood caused by storms wrought havoc','air traffic flourished in last financial year','air pollution hits new high'],'agriculture':['water scarcity hits crops','cost effective sea water desalination would benefit agriculture']}
+	thoughtnetevocations=hypervertices_intersections(thoughtnet_hypergraph,['water','air'])
+	thoughtnetevocations=hypervertices_intersections(thoughtnet_hypergraph,['water','agriculture'])
+	thoughtnetevocations=hypervertices_intersections(thoughtnet_hypergraph,['water','air','agriculture'])
