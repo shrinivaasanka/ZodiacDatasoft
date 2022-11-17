7f1f07bc7f281be61d6584eea25a5932b66d7d9f
root <NeuronRain Commits>
2022-11-17 16:45:57 +0530

    +1357. (THEORY and FEATURE) OS Scheduling and Hashing Dynamic Sets - Python 3.11 implementation of Survival Index Deadline Scheduler - 17 November 2022 - related to 751,1354 and all sections on Set partitions, Computational geometry, Program analysis/Software analytics/Scheduler Analytics among other topics in NeuronRain Theory Drafts, Sun Microsystems-Oracle-Global Analytics Patent Disclosures and Timeout implementations, Machine Learning enabled VIRGO Linux Kernel for Massive Datasets on Cloud

--- course_material/Programming/Python/code/SurvivalIndexScheduler.py
+++ course_material/Programming/Python/code/SurvivalIndexScheduler.py
@@ -17,6 +17,9 @@ import random
 import time
 import sys
 import asyncio
+import subprocess
+import os
+import json
 
 dynamic_hashmap_dict=defaultdict(list)
 process_queue=deque()
@@ -47,6 +50,7 @@ def survival_index_scheduler():
 #async def survival_index_scheduler():
     global elapsedclockticks
     while True:
+        kernelanalyticsf=open("/etc/virgo_kernel_analytics.conf","a")
         time.sleep(1)
         #await asyncio.sleep(1)
         try:
@@ -56,20 +60,45 @@ def survival_index_scheduler():
             continue
         dynamic_hashmap_dict[process[1]].append(process[0])
         print("survival_index_scheduler():",dynamic_hashmap_dict)
+        for k,v in dynamic_hashmap_dict.items():
+            kernelanalyticsf.write(str(k) + " ---- " + str(v) + "\n")
+        kernelanalyticsf.write(" --------------------------------------------- ")
+        kernelanalyticsf.write("\n")
+        kernelanalyticsf.close()
 
-def process_stream(numprocesses):
+def process_stream():
 #async def process_stream():
-    for n in range(numprocesses):
-        process_id=random.randint(1,10000)
-        deadline=random.randint(30,50)
-        process_queue.append((process_id,deadline))
+    processes=subprocess.run(["ps","-eafo","pid,cls"],capture_output=True)
+    pidsched=processes.stdout.decode("utf-8").strip().split("\n")
+    for p in pidsched[1:]:
+        ptoks=p.split()
+        process_id=ptoks[0]
+        sched_policy=ptoks[1]
+        if sched_policy=="DLN":
+            schedparams=subprocess.run(["chrt","-p",process_id],capture_output=True)
+            print(schedparams)
+            schedparamstoks=schedparams.stdout.decode("utf-8").strip().split("\n")
+            deadlinestring=schedparamstoks[2].split(":")
+            deadlinestringtoks=deadlinestring[1].split("/")
+            print("deadlinestringtoks:",deadlinestringtoks)
+            deadline=deadlinestringtoks[1]
+            process_queue.append((process_id,int(deadline)))
         print("process_stream(): process_queue = ", process_queue)
         time.sleep(1)
         #await asyncio.sleep(1)
 
+def spawn_processes(no_of_processes):
+    #SCHED_DEADLINE chrt example (runtime < deadline < period):
+    #----------------------------------------------------------
+    #chrt -d --sched-runtime 1000000 --sched-deadline 5000000 --sched-period 5000000 -p 0 <pid>
+    #chrt -d --sched-runtime 1000000 --sched-deadline 5000000 --sched-period 5000000 0 <processname>
+    for n in range(no_of_processes):
+        os.system("chrt -d --sched-runtime 1000000 --sched-deadline 5000000 --sched-period 5000000 0 python3.11 SurvivalIndexScheduler_process.py&")
+
 if __name__=="__main__":
     sys.setswitchinterval(1.0)
-    thread1 = Thread(target=process_stream,args=[10])
+    spawn_processes(20)
+    thread1 = Thread(target=process_stream)
     thread2 = Thread(target=clockticks)
     thread3 = Thread(target=survival_index_scheduler)
     thread4 = Thread(target=countdown_keys)
---
+++ course_material/Programming/Python/code/SurvivalIndexScheduler_process.py
@@ -0,0 +1,5 @@
+import sys
+
+x=0
+while x < sys.maxsize:
+	x = x + 1
