8219b273e7bbd783c4fec00ba00e85fac4991aaf
root <NeuronRain Commits>
2023-01-25 17:00:57 +0530

    1380. (THEORY and FEATURE) String Analytics and Intrinsic Merit of Music - Fisher-Yates-Knuth Shuffle augmented by Reservoir s ampling - permutation catalog of all possible music waveforms - related to 3,110,134,298,572,666,1223,1224,1257,1258,1261,1305, 1306,1307,1308,1309,1310,1311,1315,1351,1353,1378 and all sections on Music search-Music discovery, Intrinsic Merit of Music, M IR(Music Information Retrieval), Music Similarity and Clustering, AI Music Synthesis from summation of damped sinusoids, Additi ve Synthesis, fMRI connectomes - OpenNeuro - Psychology of music, Weighted automata, Chaos, Deep Learning, Dynamic Time Warping -Music Timeseries analysis, Music Evoked Autobiographical Memories, Waveshaping, ThoughtNet, Majority Voting (Enjoyable music) Vs Intrinsic (Quality music) - Condorcet Jury Theorem variants,Computational Learning Theory, Boolean Function Composition, Lon gest Common Subsequence, Suffix Trees - Longest Repeated Substring, MultiFractal Detrended Fluctuation Analysis of Non-stationa ry timeseries,Granger causality of music waveforms, Definition of Creativity, Pseudorandom permutations and Pseudorandom (numbe r) generators - Mersenne Twister, Leaky bucket algorithm, Boolean composition of Majority circuit and Voter SAT,Majority Voting  circuit-Psephology-Sampling of population for Opinion mining,Bertrand Ballot Theorem - 25 January 2023

--- course_material/Programming/C++/code/shuffle.cpp
+++ course_material/Programming/C++/code/shuffle.cpp
@@ -5,15 +5,50 @@
 
 using namespace std;
 void fisher_yates_knuth_shuffle(string s);
+void reservoir_sampling(string& s,string& r,int k);
 
 int main()
 {
-	string s="CDEFGAB";
+	string s;
+	string r;
+	int k;
+	cout<<"String to be shuffled (mostly a 12 notes octave sequence of CDEFGAB):"<<endl;
+	cin>>s;
+	cout<<"Length of string R:"<<endl;
+	cin>>k;
 	for(auto i=0; i <=10; i++)
 	{
 		cout<<"------- Shuffle "<<i<<"--------"<<endl;
 		fisher_yates_knuth_shuffle(s);
+		r.resize(k);
+		reservoir_sampling(s,r,k);
 	}
+}
+
+void reservoir_sampling(string& s,string& r,int k)
+{
+	random_device dev;
+	mt19937 rng(dev());
+	cout<<"-------------- Reservoir sampling -----------------"<<endl;
+	cout<<"Before reservoir sampling - String S:"<<s<<endl;
+	cout<<"Before reservoir sampling - String R:"<<r<<endl;
+	for(auto i=0;i <= k;i++)
+	{
+		r[i] = s[i];
+	}
+	int slen=s.length();
+	for(auto i=k+1;i < slen; i++)
+	{
+		uniform_int_distribution<mt19937::result_type> dist(1,i);
+		int randindex = dist(rng);
+		if(randindex < k)
+		{
+			cout<<"random index:"<<randindex<<endl;
+			r[randindex]=s[i];
+		}
+	}
+	cout<<"After reservoir sampling - String S:"<<s<<endl;
+	cout<<"After reservoir sampling - String R:"<<r<<endl;
 }
 
 void fisher_yates_knuth_shuffle(string s)
