da0195e6b55cb79c5d67106d051ca3777b0b81fb
root <NeuronRain Commits>
2023-01-31 16:12:51 +0530

    1383. (THEORY and FEATURE) String Analytics - String reversals by XOR swap algorithm - definition of separate Go function for reversing a string by XOR swap - related to 1380,1382 and all sections on String analytics, Large Scale Visuals Analytics, Stri ng mirroring (topological reverse of string) - Python 3.11 Matrix mirror utility primitive - based on O(1) NumPy fliplr() memor y view implementation - 31 January 2023

--- course_material/Programming/Go/multiplereturns.go
+++ course_material/Programming/Go/multiplereturns.go
@@ -23,6 +23,30 @@ func xor_swap_goroutine(x1,y1 int,channelx,channely chan int) {
 	channely <- y1 
 }
 
+func string_reverse(s string) (string) {
+	sslice:=make([]byte,len(s),len(s)+1)
+	var x1,y1 byte
+	for i := 0; i < int(len(s)/2) ; i++ {
+		x1 = s[i]
+		y1 = s[len(s) - i - 1]
+		fmt.Printf("string_reverse():before XOR - x : %x - y : %x \n",x1,y1)
+		x1 = x1 ^ y1
+		y1 = x1 ^ y1
+		x1 = x1 ^ y1
+		fmt.Printf("string_reverse():after XOR - x : %x - y : %x \n",x1,y1)
+		sslice[i] = x1
+		sslice[len(s) - i - 1] = y1
+	}
+	for i,runecodepoint := range s {
+		fmt.Printf("string_reverse(): string %d = %x\n",i,runecodepoint)
+	}
+	for i,runecodepoint := range sslice {
+		fmt.Printf("string_reverse(): reversed string %d = %x\n",i,runecodepoint)
+	}
+	fmt.Printf("string_reverse() of %s = %s\n",s,string(sslice))
+	return string(sslice)
+}
+
 func main() {
 	var x,y int =1,2
 	fmt.Printf("x : %d - y : %d \n",x,y)
@@ -34,4 +58,5 @@ func main() {
 	x= <-channelx	
 	y= <-channely	
 	fmt.Printf("after xor_swap_goroutine() === x : %d - y : %d \n",x,y)
+	string_reverse("string")
 }
