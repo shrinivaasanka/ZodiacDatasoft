3577a265fede7a6fa2dfe2d8f8d7201adde23af8
root <NeuronRain Commits>
2023-08-17 17:19:16 +0530

    +1446. (THEORY and FEATURE) Computational Geometric Factorization - High Performance Computing implementation for Supercomputers - Rayon Parallel iterator Rasterization or rectification of hyperbolic arc and Factor point location in Rust- Sequential and Parallel versions - multiple consecutive integers - Major rewrite for 64-bits integer support and (29 and 59) bits integer performance benchmarks - related to 1383,1384,1385 and all sections on Integer Factorization,Computational Geometry,Set Partitions-to-Tile Cover reduction by Lagranges Four Square Theorem and ILP,Primality Testing,RSA Semiprimes,Almost-Square Bounding Boxes in Large Scale Visual Recognition,Pell's Equation,Brahmagupta's Chakravala,Approximate Factoring by Newton-Raphson Rayshooting queries,Algebraic Geometry of Computational Geometric Factorization,Quantum computation and Derandomization of BQP Factorization by Condorcet Jury Theorem Majority-BQPVoterSAT boolean composition - 17 August 2023

--- course_material/Programming/Rust/rasterization/src/main.rs
+++ course_material/Programming/Rust/rasterization/src/main.rs
@@ -6,15 +6,25 @@ use foreach::*;
 
 fn main() {
     let args: Vec<String>=env::args().collect();
-    let number_to_factorize: i32=args[1].parse().unwrap(); 
-    let range: i32=args[2].parse().unwrap();
+    let number_to_factorize: i64=args[1].parse().unwrap(); 
+    let range: i64=args[2].parse().unwrap();
     let rasterization: String=args[3].clone();
     factorize_multipleintegers(number_to_factorize,range,rasterization);
     stdout().flush().unwrap();
+    //pariter64bits();
 }
 
-fn factorize_multipleintegers(num_fact:i32,range:i32,rasterization:String) {
+fn pariter64bits() {
+    let mut len : i64 = 10000;
+    let mut vector = 1 .. len;
+    vector.par_bridge().for_each(|paritem| {
+        println!("item {:#?} :",paritem);
+    });
+}
+
+fn factorize_multipleintegers(num_fact:i64,range:i64,rasterization:String) {
     let mut i = 0;
+    let mut runtimes = Vec::new();
     for i in 1..range {
         let mut systemtimebegin = SystemTime::now();
         let mut number_to_factorize = num_fact+i;
@@ -24,11 +34,12 @@ fn factorize_multipleintegers(num_fact:i32,range:i32,rasterization:String) {
         let systemtimeend = SystemTime::now();
         let duration = systemtimeend.duration_since(systemtimebegin).unwrap(); 
         println!("Factorization of {number_to_factorize} was completed in (nanoseconds): {:#?} ",duration);
+        runtimes.push(duration);
     }
 }
 
 #[inline]
-fn rasterize_hyperbolic_arc(num_fact: i32,rasterization: String) -> i32
+fn rasterize_hyperbolic_arc(num_fact: i64,rasterization: String) -> i64
 {
     let mut y1 = 1..num_fact-1;
     let mut y2 = 1..num_fact-1;
@@ -49,12 +60,26 @@ fn rasterize_hyperbolic_arc(num_fact: i32,rasterization: String) -> i32
     if rasterization=="parallel"
     {
         let systemtimebegin = SystemTime::now();
-        y2.into_par_iter().for_each(|paritem| {
-            let mut xtile_start = num_fact/paritem;
-            let mut xtile_end = num_fact/(paritem+1);
-            //println!("tile segment {paritem}: from {xtile_start} to {xtile_end}");
-            binary_search(num_fact,xtile_end,paritem,xtile_start,paritem);
-        });
+        let numberofbits = num_fact.ilog2();
+        println!("Size of integer to be factorized (in bits): {numberofbits}");
+        if numberofbits <= 32 
+        {
+            y2.into_par_iter().for_each(|paritem| {
+                let mut xtile_start = num_fact/paritem;
+                let mut xtile_end = num_fact/(paritem+1);
+                //println!("tile segment {paritem}: from {xtile_start} to {xtile_end}");
+                binary_search(num_fact,xtile_end,paritem,xtile_start,paritem);
+            });
+        }
+        else
+        {
+            y2.par_bridge().for_each(|paritem| {
+                let mut xtile_start = num_fact/paritem;
+                let mut xtile_end = num_fact/(paritem+1);
+                //println!("tile segment {paritem}: from {xtile_start} to {xtile_end}");
+                binary_search(num_fact,xtile_end,paritem,xtile_start,paritem);
+            });
+        }
         let systemtimeend = SystemTime::now();
         let duration = systemtimeend.duration_since(systemtimebegin).unwrap(); 
         println!("Parallel Rasterization phase of factoring {num_fact} was completed in (nanoseconds): {:#?} ",duration);
@@ -64,7 +89,7 @@ fn rasterize_hyperbolic_arc(num_fact: i32,rasterization: String) -> i32
 }
 
 #[inline]
-fn binary_search(num_fact:i32,xl:i32,yl:i32,xr:i32,yr:i32) -> Vec<i32> 
+fn binary_search(num_fact:i64,xl:i64,yl:i64,xr:i64,yr:i64) -> Vec<i64> 
 {
     //println!("binary seach of rasterized hyperbolic arc bow tilesegment xy = {num_fact}: {xl},{yl},{xr},{yr}");
     let mut xl_clone = xl.clone();
@@ -72,7 +97,7 @@ fn binary_search(num_fact:i32,xl:i32,yl:i32,xr:i32,yr:i32) -> Vec<i32>
     let mut xr_clone = xr.clone();
     let mut yr_clone = yr.clone();
     let mut factors = Vec::new();
-    while xl_clone <= xr_clone  
+    while xl_clone <= xr_clone
     {
         let mut midpoint = (xl_clone + xr_clone) / 2;
         //println!("Midpoint = {midpoint}");
@@ -86,18 +111,22 @@ fn binary_search(num_fact:i32,xl:i32,yl:i32,xr:i32,yr:i32) -> Vec<i32>
         }
         if factorcandidate==num_fact
         {
-             println!("Factor point located : {midpoint} {yl_clone}");
+             let systemtimenow = SystemTime::now();
+             println!("Factor point located : {midpoint} , {yl_clone} at {:#?} nanoseconds",systemtimenow);
              factors.push(factorcandidate);
         }
         if xl_clone*yl_clone==num_fact
         {
-             println!("Factor point located : {xl_clone} {yl_clone}");
+             let systemtimenow = SystemTime::now();
+             println!("Factor point located : {xl_clone} , {yl_clone} at {:#?} nanoseconds",systemtimenow);
              factors.push(xl_clone);
              factors.push(yl_clone);
         }
         if xr_clone*yr_clone==num_fact
         {
-             println!("Factor point located : {xr_clone} {yr_clone}");
+
+             let systemtimenow = SystemTime::now();
+             println!("Factor point located : {xr_clone} , {yr_clone} at {:#?} nanoseconds",systemtimenow);
              factors.push(xr_clone);
              factors.push(yr_clone);
         }
