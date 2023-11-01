f0a1a0a81b7ad0c749155c7b75fbdfce8886bda1
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2023-11-01 23:28:08 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- 1457. (THEORY and FEATURE) Digital Watermarking of Large Scale Visuals in OpenCV - refactoring for multiple images overlay and Image Quality Analysis(IQA) related to 581,840,841 and all sections on Merit of Large Scale Visuals, Large Scale Visual Recognition Analytics, Handwriting Recognition and Fraud analytics,GIS Urban sprawl analytics - 1 November 2023 -------------------------------------------------------------------------------------------------------------------------------------------- 1457.1 course_material/NeuronRain/LinuxKernelAndCloud/code/DigitalWatermarking.py has been updated to import image-quality and PIL python packages for image quality analysis(IQA). 1457.2 New function def image_quality_analysis(imagefile,model_path,range_path) has been defined which reads an image file in OpenCV2 and PIL and computes variance of Laplacian (Divergence of Gradient) and BRISQUE image quality measures of the image from OpenCV2 and image-quality respectively. BRISQUE functions from https://github.com/opencv/opencv_contrib/tree/master/modules/quality have been commented as alternatives which requires opencv-contrib to be built as part of OpenCV2. IQA algorithms for image quality are precisely meant for what "merit" or qualitative assessment of a visual implies. On similar lines, merit of an audio requires an AQA (audio quality analysis). 1457.3 Function def watermark_image(imagefile=None, watermark=None, fileIO=False, img=None, alpha=0.2) has been rewritten to include new arguments img,alpha and fileIO to facilitate computation of multiple image overlays. Earlier implementation was restricted to watermarking of one image by the other by addWeighted() overlay which has been extended to multiple image overlay in this update. This rewrite chooses between an imagefile name or image ndarray passed to compute overlays and returns the overlay image ndarray. Returned overlay image ndarray can be watermarked iteratively with multiple images in succession. 1457.4 Hardcoded blending parameter alpha has been made a function argument. BGRA to BGR downscaling has been commented to retain alpha channel as addWeighted() requires 4 in third axis. 1457.5 Two Lunar eclipse imagery dt.29 October 2023 from https://en.wikipedia.org/wiki/October_2023_lunar_eclipse shot from Bandung (Indonesia) and Kumbakonam (India) are IQA analysed for Laplacian blur and BRISQUE measure. BRISQUE requires download of yml files from https://github.com/opencv/opencv_contrib/tree/master/modules/quality/samples :     image_quality_analysis("testlogs/29Oct2023_LunarEclipse_Bandung_Wikipedia.jpg","testlogs/brisque_model_live.yml","testlogs/brisque_range_live.yml")     image_quality_analysis("testlogs/29Oct2023_LunarEclipse_Kumbakonam_Wikipedia.jpg","testlogs/brisque_model_live.yml","testlogs/brisque_range_live.yml") 1457.6 Four multimodal transport GIS images from https://cumta.tn.gov.in/ (Chennai Unified Metropolitan Transport Authority) have been overlaid in 3 consecutive calls to watermark_image() to create a multimodal overlay image testlogs/CMA_CUMTA_Overlay_Maps/metro_network_Watermarked.jpg. Most tools based on OpenStreetMap (OSMnx etc.,) focus mainly on road transport and overlaying the road transport with other modes of suburban transport gives a better estimate of multimodal density:     overlayimg = watermark_image(         imagefile="testlogs/CMA_CUMTA_Overlay_Maps/metro_network.jpg", watermark="testlogs/CMA_CUMTA_Overlay_Maps/mtc_network.jpg")     overlayimg = watermark_image(         watermark="testlogs/CMA_CUMTA_Overlay_Maps/rail_network.jpg", fileIO=False, img=overlayimg)     overlayimg = watermark_image(         watermark="testlogs/CMA_CUMTA_Overlay_Maps/road_network.jpg", fileIO=False, img=overlayimg)     cv2.imwrite("testlogs/CMA_CUMTA_Overlay_Maps/metro_network_Watermarked.jpg", overlayimg)

--- course_material/NeuronRain/LinuxKernelAndCloud/code/DigitalWatermarking.py
+++ course_material/NeuronRain/LinuxKernelAndCloud/code/DigitalWatermarking.py
@@ -1,62 +1,95 @@
 ##############################################################################################################################################
-#<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+# <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
 ##############################################################################################################################################
-#Course Authored By:
-#-----------------------------------------------------------------------------------------------------------
-#K.Srinivasan
-#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
-#Personal website(research): https://sites.google.com/site/kuja27/
-#-----------------------------------------------------------------------------------------------------------
+# Course Authored By:
+# -----------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://sites.google.com/site/kuja27/
+# -----------------------------------------------------------------------------------------------------------
 
-import cv2 
+import cv2
 import numpy as np
+import imquality.brisque as brisque
+import PIL.Image
 
-def watermark_image(imagefile, watermark):
-	img = cv2.imread(imagefile,-1)
-	img = cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
-	namewatermark = cv2.imread(watermark,-1)
-	namewatermark = cv2.cvtColor(namewatermark,cv2.COLOR_BGR2BGRA)
-	imgheight,imgwidth,imgc = img.shape
-	nwmheight,nwmwidth,nwmc = namewatermark.shape
-	overlay = np.zeros((imgheight,imgwidth,4), dtype='uint8')	
-	for p in range(0,nwmheight):
-		for q in range(0,nwmwidth):
-			if namewatermark[p,q][3] != 0:
-				heightoffset = imgheight - nwmheight - 100
-				widthoffset = imgwidth - nwmwidth - 100
-				#print(heightoffset+p,",",widthoffset+q)
-				overlay[heightoffset + p, widthoffset + q] = namewatermark[p,q]
-	cv2.addWeighted(overlay, 0.05, img, 0.95, 0, img) 
-	img = cv2.cvtColor(img,cv2.COLOR_BGRA2BGR)
-	cv2.imwrite(imagefile+"_Watermarked.jpg",img)
-	
+def image_quality_analysis(imagefile,model_path,range_path):
+    img = cv2.imread(imagefile,cv2.IMREAD_COLOR)
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.Laplacian(grey, cv2.CV_64F).var()
+    #score = cv2.QualityBRISQUE_compute(img, model_path, range_path) 
+    #obj = cv2.quality.QualityBRISQUE_create(model_path, range_path)
+    #score = obj.compute(img)
+    pilimg=PIL.Image.open(imagefile)
+    score = brisque.score(pilimg)
+    print(imagefile + " - brisque IQA score:",score)
+    print(imagefile + " - blur:",blur)
+
+def watermark_image(imagefile=None, watermark=None, fileIO=False, img=None, alpha=0.2):
+    if imagefile is not None:
+        img = cv2.imread(imagefile, -1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+    namewatermark = cv2.imread(watermark, -1)
+    namewatermark = cv2.cvtColor(namewatermark, cv2.COLOR_BGR2BGRA)
+    imgheight, imgwidth, imgc = img.shape
+    nwmheight, nwmwidth, nwmc = namewatermark.shape
+    overlay = np.zeros((imgheight, imgwidth, 4), dtype='uint8')
+    for p in range(0, nwmheight):
+        for q in range(0, nwmwidth):
+            if namewatermark[p, q][3] != 0:
+                heightoffset = imgheight - nwmheight - 100
+                widthoffset = imgwidth - nwmwidth - 100
+                # print(heightoffset+p,",",widthoffset+q)
+                overlay[heightoffset + p, widthoffset +
+                        q] = namewatermark[p, q]
+    print("overlay shape:",overlay.shape)
+    print("image shape:",img.shape)
+    cv2.addWeighted(overlay, alpha, img, 1-alpha, 0, img)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    if fileIO:
+        cv2.imwrite(imagefile+"_Watermarked.jpg", img)
+        return img 
+    else:
+        return img 
+
 
 def watermark_video(videofile, watermark, maxframes):
-	namewatermark = cv2.imread(watermark,-1)
-	namewatermark = cv2.cvtColor(namewatermark,cv2.COLOR_BGR2BGRA)
-	vid = cv2.VideoCapture(videofile)
-	cnt=1
-	while True and cnt <= maxframes:
-		ret, frame = vid.read()
-		frame = cv2.cvtColor(frame,cv2.COLOR_BGR2BGRA)
-		frameheight,framewidth,framec = frame.shape
-		nwmheight,nwmwidth,nwmc = namewatermark.shape
-		overlay = np.zeros((frameheight,framewidth,4), dtype='uint8')	
-		for p in range(0,nwmheight):
-			for q in range(0,nwmwidth):
-				if namewatermark[p,q][3] != 0:
-					heightoffset = frameheight - nwmheight - 100
-					widthoffset = framewidth - nwmwidth - 100
-					#print(heightoffset+p,",",widthoffset+q)
-					overlay[heightoffset + p, widthoffset + q] = namewatermark[p,q]
-		cv2.addWeighted(overlay, 0.05, frame, 0.95, 0, frame) 
-		frame = cv2.cvtColor(frame,cv2.COLOR_BGRA2BGR)
-		cv2.imwrite(videofile+"_Frame%d.jpg" % (cnt),frame)
-		cnt += 1
+    namewatermark = cv2.imread(watermark, -1)
+    namewatermark = cv2.cvtColor(namewatermark, cv2.COLOR_BGR2BGRA)
+    vid = cv2.VideoCapture(videofile)
+    cnt = 1
+    while True and cnt <= maxframes:
+        ret, frame = vid.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        frameheight, framewidth, framec = frame.shape
+        nwmheight, nwmwidth, nwmc = namewatermark.shape
+        overlay = np.zeros((frameheight, framewidth, 4), dtype='uint8')
+        for p in range(0, nwmheight):
+            for q in range(0, nwmwidth):
+                if namewatermark[p, q][3] != 0:
+                    heightoffset = frameheight - nwmheight - 100
+                    widthoffset = framewidth - nwmwidth - 100
+                    # print(heightoffset+p,",",widthoffset+q)
+                    overlay[heightoffset + p, widthoffset +
+                            q] = namewatermark[p, q]
+        cv2.addWeighted(overlay, 0.05, frame, 0.95, 0, frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+        cv2.imwrite(videofile+"_Frame%d.jpg" % (cnt), frame)
+        cnt += 1
 
-if __name__=="__main__":
-	watermark_video("testlogs/Krishna_iResearch_NeuronRain_Repositories-2020-07-10_13.17.20.mp4","testlogs/DigitalWatermarking_Name.jpg",2)
-	watermark_image("testlogs/DWMExample1.jpg","testlogs/DigitalWatermarking_PSGTech.jpg")
-	watermark_image("testlogs/DWMExample2.jpg","testlogs/DigitalWatermarking_PSGTech.jpg")
-	watermark_image("testlogs/DWMExample1.jpg","testlogs/DigitalWatermarking_PSGTech.png")
-	watermark_image("testlogs/DWMExample2.jpg","testlogs/DigitalWatermarking_PSGTech.png")
+
+if __name__ == "__main__":
+    # watermark_video("testlogs/Krishna_iResearch_NeuronRain_Repositories-2020-07-10_13.17.20.mp4","testlogs/DigitalWatermarking_Name.jpg",2)
+    # watermark_image("testlogs/DWMExample1.jpg","testlogs/DigitalWatermarking_PSGTech.jpg")
+    # watermark_image("testlogs/DWMExample2.jpg","testlogs/DigitalWatermarking_PSGTech.jpg")
+    # watermark_image("testlogs/DWMExample1.jpg","testlogs/DigitalWatermarking_PSGTech.png")
+    # watermark_image("testlogs/DWMExample2.jpg","testlogs/DigitalWatermarking_PSGTech.png")
+    image_quality_analysis("testlogs/29Oct2023_LunarEclipse_Bandung_Wikipedia.jpg","testlogs/brisque_model_live.yml","testlogs/brisque_range_live.yml")
+    image_quality_analysis("testlogs/29Oct2023_LunarEclipse_Kumbakonam_Wikipedia.jpg","testlogs/brisque_model_live.yml","testlogs/brisque_range_live.yml")
+    overlayimg = watermark_image(
+        imagefile="testlogs/CMA_CUMTA_Overlay_Maps/metro_network.jpg", watermark="testlogs/CMA_CUMTA_Overlay_Maps/mtc_network.jpg")
+    overlayimg = watermark_image(
+        watermark="testlogs/CMA_CUMTA_Overlay_Maps/rail_network.jpg", fileIO=False, img=overlayimg)
+    overlayimg = watermark_image(
+        watermark="testlogs/CMA_CUMTA_Overlay_Maps/road_network.jpg", fileIO=False, img=overlayimg)
+    cv2.imwrite("testlogs/CMA_CUMTA_Overlay_Maps/metro_network_Watermarked.jpg", overlayimg)
