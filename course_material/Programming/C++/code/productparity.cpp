0e460f57de73ea12210b8c1d5883d324a50205c1
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2023-12-14 23:16:30 +0530

    +-------------------------------------------------------------------------------------------------------------------------------------------- +1466. (THEORY and FEATURE) Binary Product of 2 integers by C++ STL Bitset and relation between parity of factors and parity of product - multiple integers - 15 December 2023 - related to all sections on computational geometric integer factorization +-------------------------------------------------------------------------------------------------------------------------------------------- +1466.1 course_material/Programming/C++/code/productparity.cpp has been updated to introduce 2 nested for loops for iterating through the range of commandline arguments x and y and compute parity of the products for entire range of 1...x and 1...y. +1466.2 Parities of the products are plotted as ferrer diagram (code/logs/productparity.log.15December2023). Boolean complexity theoretic problem of relation between parity(x*y) and [parity(x) and parity(y)] is partially answered by lemmas 8.9,8.10 and 8.11 - https://courses.cs.washington.edu/courses/cse532/08sp/lect08.pdf - "Theorem 8.8 Parity ∈ NC1 , and Integer-Addition ∈ AC0....Theorem 8.9 Iterated-Addition of n n-bit integers ∈ NC1.... By taking an n-bit input x, producing x' = xn0lxn−10l · · · 0lx1 where l > log2 n and multiplying this by y' = 10l10l · · · 0l1 and taking the middle bit of x' · y' we derive the following: Lemma 8.11 Parity ≤NC0 Multiplication ...." - in other words, parity of an integer of the form described above is the middle bit of product of its factors. +1466.3 Multiplication of 2 n-bit integers can be written as Wallace tree reduction of x + y + z = u + v recursively by expressing carry bit as logic formula : ".... The sum i-th bit of the sum si = xi ⊕ yi ⊕ ci where ci is the carry bit. The i-th carry bit ci is 1 iff it is generated in some column j to the right of column i, and is propagated in every column between j and i. That is, ci = ∨j<i[(xj ∧ yj ) ∧ ∧j<k<i(xk ∨ yk)] which yields a polynomial-size constant depth unbounded fan-in circuit ...."

--- course_material/Programming/C++/code/productparity.cpp
+++ course_material/Programming/C++/code/productparity.cpp
@@ -12,12 +12,36 @@ int productparity(unsigned long long a,unsigned long long b);
 
 int main(int argc, char* argv[])
 {
+	vector<int> productparities;	
 	char* end;
-	productparity(strtoull(argv[1],&end,10),strtoull(argv[2],&end,10));
+	unsigned long long x = strtoull(argv[1],&end,10);
+	unsigned long long y = strtoull(argv[2],&end,10);
+	for(unsigned long long i = 1; i <= x; i++)
+	{
+		for(unsigned long long j = 1; j <= y; j++)
+		{
+			int prod_par=productparity(i,j);
+			productparities.push_back(prod_par);
+		}
+	}
+	cout<<"======================================================"<<endl;
+	cout<<"Produt Parity graph - plotted as Ferrer diagram"<<endl;
+	cout<<"======================================================"<<endl;
+	for(vector<int>::iterator it=productparities.begin();it != productparities.end();it++)
+	{
+		for(int i=1;i <= *it;i++)
+		{
+			cout<<"#";
+		}
+		cout<<endl;
+	}
 }
 
 int productparity(unsigned long long a,unsigned long long b) 
 {
+	cout<<"==============================================="<<endl;
+	cout<<"Product Parity of "<<a<<" * "<<b<<endl;
+	cout<<"==============================================="<<endl;
 	bitset<64> x(a);
 	bitset<64> y(b);
 	cout<<"binary x:"<<x<<" -- parity(number of 1s) -- "<<x.count()<<endl;
@@ -25,4 +49,5 @@ int productparity(unsigned long long a,unsigned long long b)
 	unsigned long long c=a*b;
 	bitset<64> z(c);
 	cout<<"binary x*y:"<<z<<" -- parity(number of 1s) -- "<<z.count()<<endl;
+	return z.count();
 }
