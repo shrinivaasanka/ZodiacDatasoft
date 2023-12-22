038ad4d5b8c6071845be17a7228d47f6d9d75425
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2023-12-22 15:51:44 +0530

    +1467. (THEORY and FEATURE) Russian Peasant Multiplication in Linear Time by BitShift operator - Approximate parity based estimate 3 of last row of the tableau for an alternative non-trivial reversal of multiplication (or factoring) - related to 668,1221,1222,1429,1465,1466 and all sections on Integer Factorization, Succinct Datastrucures, Numeric Compression, Arithmetic Circuits, Computational Geometric Factorization - Average case Sequential Optimization, Stirling Approximation of Gamma Function, Relation between parity of an integer and its factors - 22 December 2023

--- course_material/Programming/C++/code/russianpeasant.cpp
+++ course_material/Programming/C++/code/russianpeasant.cpp
@@ -5,10 +5,17 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
+extern "C"
+{
+#include "math.h"
+}
+
 int russian_peasant_linear_multiplication(unsigned long long a,unsigned long long b,double lowerboundepsilon);
+unsigned long long computeLSB(int approx_parity,int leftshift);
 
 int main(int argc, char* argv[])
 {
@@ -26,22 +33,51 @@ int russian_peasant_linear_multiplication(unsigned long long a,unsigned long lon
 	cout<<"==================================="<<endl;
 	while(true)
 	{
-		cout<<"tempa (factor1 / 2^"<<count<<"):"<<tempa<<" ------ tempb (factor2 * 2^"<<count<<"):"<<tempb<<endl;
 		if(tempa % 2 == 1)
 		{
 			product_ab += tempb;
+			cout<<"tempa (factor1 / 2^"<<count<<"):"<<tempa<<" ------ tempb (factor2 * 2^"<<count<<"):"<<tempb<<" ============= ODD Left Column ==================="<<endl;
 			if(tempa == 0 or tempa == 1)
 				break;
+		}
+		else
+		{
+			cout<<"tempa (factor1 / 2^"<<count<<"):"<<tempa<<" ------ tempb (factor2 * 2^"<<count<<"):"<<tempb<<endl;
 		}
 		tempa = tempa >> 1;
 		tempb = tempb << 1;
 		count += 1;
 	}
 	double lowerbound1 = (double) (std::log(a*b)/std::log(2)) - lowerboundepsilon;
+	cout<<"Length of Russian peasant tableau:"<<count<<endl;
 	cout<<"Factor1 size(in bits):"<<std::log(a)/std::log(2)<<endl;
 	cout<<"Factor2 size(in bits):"<<std::log(b)/std::log(2)<<endl;
 	double lowerbound2 = (double) (std::log(a)/std::log(2) + std::log(b)/std::log(2) - lowerboundepsilon);
+	bitset<64> ab(product_ab);
+	cout<<"ab.count():"<<ab.count()<<endl;
+	int approx_parity = ab.count() >> 1 ;
+	cout<<"Approximate Parity of left column (factor1) till penultimate row = ab.count()/2 :"<<approx_parity<<endl;
+	int leftshift = count - approx_parity;
+	cout<<"Leftshift(ab.count() - approximate_parity):"<<leftshift<<endl;
+	unsigned long long leastsignificantbyte = computeLSB(approx_parity,leftshift);
+	cout<<"Least significant byte:"<<leastsignificantbyte<<endl;
+	unsigned long long lowerbound3 = (unsigned long long) (product_ab - leastsignificantbyte);
+	cout<<"Lowerbound epsilon:"<<lowerboundepsilon<<endl;
 	cout<<"Approximate lower bound estimate 1 for last row,right column - 2^("<<lowerbound1<<"):"<<(unsigned long long) pow(2,lowerbound1)<<endl; 
 	cout<<"Approximate lower bound estimate 2 for last row,right column - 2^("<<lowerbound2<<"):"<<(unsigned long long) pow(2,lowerbound2)<<endl; 
-	cout<<"Product of "<<a<<" and "<<b<<":"<<product_ab<<endl;
+	cout<<"Approximate estimate 3 for last row,right column :"<<lowerbound3<<endl; 
+	cout<<"Product Factor1 * Factor2 of "<<a<<" and "<<b<<":"<<product_ab<<endl;
+}
+
+unsigned long long computeLSB(int approx_parity,int leftshift)
+{
+	unsigned long long lsb=0;
+	for(int i=0;i < approx_parity;i++)
+	{
+		cout<<"computeLSB(): lsb = "<<lsb<<endl;
+		lsb += pow(2,i); 
+	}
+	int leftshiftedlsb = lsb << leftshift;
+	cout<<"leftshiftedlsb:"<<leftshiftedlsb<<endl;
+	return leftshiftedlsb;
 }
